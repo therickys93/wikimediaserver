e2766aabd8fc1fe2e0a2335a62356b7b5e83a270
Riccardo Crippa <therickys93@gmail.com>
2018-01-24 20:28:03 +0100

    pronto per modificare la grafica

--- app.js
+++ app.js
@@ -19,14 +19,13 @@ app.get('/status', function (req, res) {
 	console.log("GET /status")
 	var response = {"success":true}
 	console.log(response)
-  	res.json(response)
+	res.json(response)
 })
 
 app.get('/files', function(req, res){
 	console.log("GET /files")
 	fs.readdir(directory, function(err, items) {
 		console.log(items)
-		// res.json(items)
 		res.render('files', { title: 'Wiki MediaServer', files: items })
 	})
 })
@@ -44,5 +43,5 @@ app.get('/search/:name', function(req, res){
 })
 
 app.listen(port, function () {
-  	console.log('Example app listening on port 3000!')
+	console.log('Wiki Media Server listening on port ' + port + '!')
 })
